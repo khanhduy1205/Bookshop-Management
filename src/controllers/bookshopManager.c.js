2c082fa93e73c469d23f4855f195de7788fd3afc
20120247 <phuonganh2002ph@gmail.com>
2023-06-15 16:29:36 +0700

    Create UI

--- src/controllers/bookshopManager.c.js
+++ src/controllers/bookshopManager.c.js
@@ -8,3 +8,25 @@ exports.getSearch = async (req, res, next) => {
     res.render('search');
 };
 
+exports.getImport = async (req, res, next) => {
+    res.render('all_imports');
+};
+
+exports.getInvoice = async (req, res, next) => {
+    res.render('invoice');
+};
+
+exports.getBills = async (req, res, next) => {
+    res.render('bill');
+};
+
+exports.getReports = async (req, res, next) => {
+    res.render('report');
+};
+
+exports.getRules = async (req, res, next) => {
+    res.render('rule');
+};
+
+
+
--- src/routers/router.r.js
+++ src/routers/router.r.js
@@ -7,6 +7,14 @@ router.get('/', controller.getHome);
 
 router.get('/search', controller.getSearch);
 
+router.get('/import', controller.getImport);
 
+router.get('/invoice', controller.getInvoice);
+
+router.get('/bill', controller.getBills);
+
+router.get('/report', controller.getReports);
+
+router.get('/rule', controller.getRules);
 
 module.exports = router;
